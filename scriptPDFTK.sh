026b79ed85d6c896b9824e6167c523a390b2a03c
Francisco Vila <paconet.org@gmail.com>
2010-04-08 14:26:29 +0200

    Week 24: book and bookpart.

---
+++ scriptPDFTK.sh
@@ -0,0 +1,5 @@
+pdftk pruebalibro.pdf cat 1 output outA1.pdf
+pdftk pruebalibro.pdf cat 2 output outA2.pdf
+pdftk pruebalibro-1.pdf cat 1 output outB1.pdf
+pdftk pruebalibro-1.pdf cat 2 output outB2.pdf
+
