766d3ecaa4b09f20d567deaf7b94cd7d432ac5a9
Paco - tomillo <paco@tomillo>
2014-10-03 16:36:29 +0200

    Update lilypond-by-example.

--- lilypond-by-example/haz.sh
+++ lilypond-by-example/haz.sh
@@ -4,6 +4,6 @@ if [ -f out/lpe.tex ] ; then rm out/lpe.tex; fi
 /bin/cp lily-logo.png out/	
 lilypond-book --pdf --output=out --format=latex lpe.tex &>log
 cd out
-pdflatex lpe.tex
+xelatex lpe.tex
 cd ..
 /bin/cp out/lpe.pdf lpe.pdf
